a9109c4e92a118acecda2dbe379d194448c77307
PCBogota <pcbogota.com@gmail.com>
2026-04-17 19:58:15 -0500

    testing apid/db.js using https 4

--- api/db.js
+++ api/db.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
       },
       body: JSON.stringify({
         requests: [
-          { type: "execute", stmt: { sql: "SELECT * FROM client WHERE id = 2" } },
+          { type: "execute", stmt: { sql: "SELECT * FROM client WHERE" } },
           { type: "close" },
         ],
       }),
