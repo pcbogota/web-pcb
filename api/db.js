5ef354811e159966f40e0401545814559759b788
PCBogota <pcbogota.com@gmail.com>
2026-04-17 19:29:29 -0500

    updated apid/db.js witouth /web in node 20.x

--- api/db.js
+++ api/db.js
@@ -1,4 +1,4 @@
-import { createClient } from "@libsql/client/web";
+import { createClient } from "@libsql/client";
 
 export default async function handler(req, res) {
   // Verificación de variables para asegurar que Node 24 las lea
